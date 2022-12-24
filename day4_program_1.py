45ac136299297706316c11133f5f35d3a261384a
Gangully321 <121158127+Gangully321@users.noreply.github.com>
2022-12-24 12:56:36 +0530

    Add files via upload

---
+++ day4_program_1.py
@@ -0,0 +1,9 @@
+student=[[83,'ajay pandey',74,83],[38,'sruthi deshmuk',93,84],[84,'jony sins',84,98]]
+student.sort()
+print(student)
+student.sort(key=lambda temp:temp[2])
+print(student)
+student.sort(key=lambda temp:temp[3])
+print(student)
+                                  
+                                  
