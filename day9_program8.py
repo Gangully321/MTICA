1eeb218387346eccd867b41c94191517031cf002
Gangully321 <121158127+Gangully321@users.noreply.github.com>
2022-12-30 17:31:24 +0530

    Add files via upload

---
+++ day9_program8.py
@@ -0,0 +1,9 @@
+gg=open(r'C:\Users\User\Desktop\DEG1.txt',"w+")
+inpStr=input('enter text:')
+gg.write(inpStr)
+inpStr=input('enter text:')
+gg.write(inpStr)
+
+gg.close()
+print('TEXT WRITTEN TO FILE')
+
